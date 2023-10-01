b400760b95ea6d4a0b075541a0b709042c8482f0
Ojas Mittal <ojasfarm31@gmail.com>
2023-10-01 10:45:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// var genName= require("sillyname");
+import genName from "sillyname";
+import superheroes from "superheroes";
+console.log(superheroes.random());
+var sillt=genName();
+console.log('My name is '+sillt);
